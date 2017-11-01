d9b343d71d7ad65ee8af10d6e60695fc1bdde5e4
Juan Manuel Martin Mora <martinmoraj@sigen.net>
2017-11-01 16:49:38 -0300

    CheckStyle
    Se continuan con las mejoras en el codigo a partir de  lo informado en checkstyle

--- src/main/java/comandos/ActualizarInventario.java
+++ src/main/java/comandos/ActualizarInventario.java
@@ -1,6 +1,7 @@
 package comandos;
 
 import mensajeria.PaquetePersonaje;
+
 /**
  * Actualiza el Inventario del personaje.
  * @author prograAvanzada
@@ -10,16 +11,16 @@ public class ActualizarInventario extends ComandosEscucha {
 
 	@Override
 	public void ejecutar() {
-		PaquetePersonaje paquetePersonaje = (PaquetePersonaje) gson.fromJson(cadenaLeida, PaquetePersonaje.class);
-		juego.getPersonajesConectados().remove(paquetePersonaje.getId());
-		juego.getPersonajesConectados().put(paquetePersonaje.getId(), paquetePersonaje);
-		if (juego.getPersonaje().getId() == paquetePersonaje.getId()) {
+		PaquetePersonaje pP;
+		pP= (PaquetePersonaje) gson.fromJson(cadenaLeida, PaquetePersonaje.class);
+		juego.getPersonajesConectados().remove(pP.getId());
+		juego.getPersonajesConectados().put(pP.getId(), pP);
+		if (juego.getPersonaje().getId() == pP.getId()) {
 			juego.actualizarPersonaje();
 			juego.getEstadoJuego().actualizarPersonaje();
-			juego.getCliente().actualizarItems(paquetePersonaje);
-			juego.getCliente().actualizarPersonaje(juego.getPersonajesConectados().get(paquetePersonaje.getId()));
+			juego.getCliente().actualizarItems(pP);
+			juego.getCliente().actualizarPersonaje(juego.getPersonajesConectados().get(pP.getId()));
 		}
-
 	}
-
+	
 }
