bcbcfbdc50afdd0163cc8e1d4a016190283ad7b9
Matii <Matii@Mats-MacBook-Pro-2.local>
2017-11-02 18:30:29 -0300

    CheckStyle ActualizarInventario.java

--- src/main/java/comandos/ActualizarInventario.java
+++ src/main/java/comandos/ActualizarInventario.java
@@ -1,7 +1,6 @@
 package comandos;
 
 import mensajeria.PaquetePersonaje;
-<<<<<<< HEAD
 /**
  * 
  * @author UnlamPrograAvanzada
@@ -18,18 +17,6 @@ public class ActualizarInventario extends ComandosEscucha {
 
 	@Override
 	public void ejecutar() {
-<<<<<<< HEAD
-		PaquetePersonaje paquetePersonaje =
-				(PaquetePersonaje) gson.fromJson(cadenaLeida, PaquetePersonaje.class);
-		juego.getPersonajesConectados().remove(paquetePersonaje.getId());
-		juego.getPersonajesConectados().put(paquetePersonaje.getId(), paquetePersonaje);
-		if (juego.getPersonaje().getId() == paquetePersonaje.getId()) {
-			juego.actualizarPersonaje();
-			juego.getEstadoJuego().actualizarPersonaje();
-			juego.getCliente().actualizarItems(paquetePersonaje);
-			juego.getCliente().actualizarPersonaje(
-					juego.getPersonajesConectados().get(paquetePersonaje.getId()));
-=======
 		PaquetePersonaje pP;
 		pP= (PaquetePersonaje) gson.fromJson(cadenaLeida, PaquetePersonaje.class);
 		juego.getPersonajesConectados().remove(pP.getId());
@@ -39,7 +26,6 @@ public class ActualizarInventario extends ComandosEscucha {
 			juego.getEstadoJuego().actualizarPersonaje();
 			juego.getCliente().actualizarItems(pP);
 			juego.getCliente().actualizarPersonaje(juego.getPersonajesConectados().get(pP.getId()));
->>>>>>> effef49f97e6a125aa58f5894f3f977cfc47255b
 		}
 	}
 	
