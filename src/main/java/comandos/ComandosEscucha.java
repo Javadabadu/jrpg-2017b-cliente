3e5926b9486d1b091e9d57c7e677b4914c7b81ec
Nicolás Casaux <nicocasaux@hotmail.com>
2017-11-02 20:37:14 -0300

    Checkstyle ComandosEscucha.java

--- src/main/java/comandos/ComandosEscucha.java
+++ src/main/java/comandos/ComandosEscucha.java
@@ -2,11 +2,17 @@ package comandos;
 
 import juego.Juego;
 import mensajeria.Comando;
-
+/**
+ * @author prograAvanzada
+ * 
+ */
 public abstract class ComandosEscucha extends Comando {
-	protected Juego juego;
-	public void setJuego(Juego juego) {
+	private Juego juego;
+	public void setJuego(final Juego juego) {
 		this.juego = juego;
+	}
+	public Juego getJuego() {
+		return juego;
 	}
 
 }
