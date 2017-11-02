31288facad6fa800c09485d81ff06c8ecb867e2a
Nicolás Casaux <nicocasaux@hotmail.com>
2017-11-01 21:29:07 -0300

    Checkstyle CargadorImagen.java

--- src/main/resources/recursos/CargadorImagen.java
+++ src/main/resources/recursos/CargadorImagen.java
@@ -5,16 +5,21 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 import javax.swing.JOptionPane;
-
+/**
+ * 
+ * @author UnlamPrograAvanzada
+ * carga la imagen
+ */
 public class CargadorImagen {
-
-	public static BufferedImage cargarImagen(String path) {
+	
+	private CargadorImagen(){
+	}
+	public static BufferedImage cargarImagen(final String path) {
 		try {
 			return  ImageIO.read(CargadorImagen.class.getResource(path));
 		} catch (IOException e) {
 			JOptionPane.showMessageDialog(null, "Error al cargar el archivo " + path);
 		}
-
 		return null;
 	}
 }
