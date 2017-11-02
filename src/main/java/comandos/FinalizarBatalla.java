5224e0ed4787b656f4a46e4815e3155d0a7e56ca
Nicolás Casaux <nicocasaux@hotmail.com>
2017-11-02 20:48:17 -0300

    Checkstyle FinalizarBatalla.java

--- src/main/java/comandos/FinalizarBatalla.java
+++ src/main/java/comandos/FinalizarBatalla.java
@@ -2,15 +2,18 @@ package comandos;
 
 import estados.Estado;
 import mensajeria.PaqueteFinalizarBatalla;
-
-public class FinalizarBatalla extends ComandosEscucha{
+/**
+ * Finaliza la batalla
+ * @author prograAvanzada
+ * 
+ */
+public class FinalizarBatalla extends ComandosEscucha {
 
 	@Override
 	public void ejecutar() {
-		PaqueteFinalizarBatalla paqueteFinalizarBatalla = (PaqueteFinalizarBatalla) gson.fromJson(cadenaLeida, PaqueteFinalizarBatalla.class);
+		PaqueteFinalizarBatalla paqueteFinalizarBatalla =
+				(PaqueteFinalizarBatalla) gson.fromJson(cadenaLeida, PaqueteFinalizarBatalla.class);
 		juego.getPersonaje().setEstado(Estado.estadoJuego);
 		Estado.setEstado(juego.getEstadoJuego());
-		
 	}
-	
 }
