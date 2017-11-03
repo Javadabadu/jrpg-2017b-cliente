360cac57e57fb7e209387761b4d447faab26440c
Nicolás Casaux <nicocasaux@hotmail.com>
2017-11-02 21:01:18 -0300

    Checkstyle ActualizarPersonajeLvl.java

--- src/main/java/comandos/ActualizarPersonajeLvl.java
+++ src/main/java/comandos/ActualizarPersonajeLvl.java
@@ -1,22 +1,22 @@
 package comandos;
 
 import mensajeria.PaquetePersonaje;
-
+/**
+ * @author Javadabadu
+ * actualiza el nivel del personaje
+ */
 public class ActualizarPersonajeLvl extends ComandosEscucha {
-
 	@Override
 	public void ejecutar() {
-		PaquetePersonaje paquetePersonaje = (PaquetePersonaje) gson.fromJson(cadenaLeida, PaquetePersonaje.class);
-		
+		PaquetePersonaje paquetePersonaje =
+				(PaquetePersonaje) gson.fromJson(cadenaLeida, PaquetePersonaje.class);
 		juego.getPersonajesConectados().remove(paquetePersonaje.getId());
 		juego.getPersonajesConectados().put(paquetePersonaje.getId(), paquetePersonaje);
-		
 		if (juego.getPersonaje().getId() == paquetePersonaje.getId()) {
 			juego.actualizarPersonaje();
 			juego.getEstadoJuego().actualizarPersonaje();
-			juego.getCliente().actualizarPersonaje(juego.getPersonajesConectados().get(paquetePersonaje.getId()));
+			juego.getCliente().actualizarPersonaje(
+					juego.getPersonajesConectados().get(paquetePersonaje.getId()));
 		}
-		
 	}
-
 }
