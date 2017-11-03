192ada3cb260b5354481c82e849eeeff6938add4
Nicolás Casaux <nicocasaux@hotmail.com>
2017-11-03 11:44:32 -0300

    Checkstyle.java

--- src/main/java/mensajeria/PaqueteMensaje.java
+++ src/main/java/mensajeria/PaqueteMensaje.java
@@ -1,40 +1,57 @@
 package mensajeria;
 
 import java.io.Serializable;
-
+/**
+ * @author Javadabadu
+ */
 public class PaqueteMensaje extends Paquete implements Serializable, Cloneable {
-	
 		private String userEmisor;
 		private String userReceptor;
 		private String msj;
-
-		public PaqueteMensaje(){
+		/**
+		 * Constructor por defecto
+		 */
+		public PaqueteMensaje() {
 		}
-
+		/**
+		 * obtiene el mensaje
+		 */
 		public String getMensaje() {
 			return msj;
 		}
-
-		public void setMensaje(String mensaje) {
+		/**
+		 * setea el mensaje
+		 */
+		public void setMensaje(final String mensaje) {
 			this.msj = mensaje;
 		}
-
+		/**
+		 * obtiene el usuario emisor
+		 */
 		public String getUserEmisor() {
 			return userEmisor;
 		}
-
-		public void setUserEmisor(String idEmisor) {
+		/**
+		 * setea el usuario emisor
+		 */
+		public void setUserEmisor(final String idEmisor) {
 			this.userEmisor = idEmisor;
 		}
-
+		/**
+		 * obtiene el usuario receptor
+		 */
 		public String getUserReceptor() {
 			return userReceptor;
 		}
-
-		public void setUserReceptor(String idReceptor){
+		/**
+		 * setea el usuario receptor
+		 */
+		public void setUserReceptor(final String idReceptor) {
 			this.userReceptor = idReceptor;
 		}
-		
+		/**
+		 * clona el objeto
+		 */
 		public Object clone() {
 			Object obj = null;
 			obj = super.clone();
