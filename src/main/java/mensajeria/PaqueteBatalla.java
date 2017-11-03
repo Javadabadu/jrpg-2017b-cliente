d56534b0256f455a3647a9ebcadbe4c81603f283
Nicolás Casaux <nicocasaux@hotmail.com>
2017-11-03 11:07:32 -0300

    Checkstyle PaqueteBatalla.java

--- src/main/java/mensajeria/PaqueteBatalla.java
+++ src/main/java/mensajeria/PaqueteBatalla.java
@@ -1,7 +1,9 @@
 package mensajeria;
 
 import java.io.Serializable;
-
+/**
+ * @author Javadabadu
+ */
 public class PaqueteBatalla extends Paquete implements Serializable, Cloneable {
 	
 	public static final int BATALLAPERSONAJE = 0;
@@ -11,48 +13,67 @@ public class PaqueteBatalla extends Paquete implements Serializable, Cloneable {
 	private int idEnemigo;
 	private boolean miTurno;
 	private int tipoBatalla;
-
-	public PaqueteBatalla(){
+	/**
+	 * Constructor por defecto
+	 */
+	public PaqueteBatalla() {
 		setComando(Comando.BATALLA);
 		tipoBatalla = BATALLAPERSONAJE;
 	}
-	
-	public PaqueteBatalla(int tipoBatalla){
+	/**
+	 * Constructor parametrizado
+	 */
+	public PaqueteBatalla(final int tipoBatalla) {
 		setComando(Comando.BATALLA);
 		this.tipoBatalla = tipoBatalla;
 	}
-
+	/**
+	 * obtiene el id
+	 */
 	public int getId() {
 		return id;
 	}
-
-
-	public void setId(int id) {
+	/**
+	 * Setea el id
+	 */
+	public void setId(final int id) {
 		this.id = id;
 	}
 
-
+	/**
+	 * obtiene el idEnemigo
+	 */
 	public int getIdEnemigo() {
 		return idEnemigo;
 	}
-
-	public void setIdEnemigo(int idEnemigo){
+	/**
+	 * setea el idEnemigo
+	 */
+	public void setIdEnemigo(final int idEnemigo) {
 		this.idEnemigo = idEnemigo;
 	}
-
+	/**
+	 * obtiene miTurno
+	 */
 	public boolean isMiTurno() {
 		return miTurno;
 	}
-
-	public void setMiTurno(boolean miTurno) {
+	/**
+	 * setea miTurno
+	 */
+	public void setMiTurno(final boolean miTurno) {
 		this.miTurno = miTurno;
 	}
-	
+	/**
+	 * obtiene el tipo de batalla
+	 */
 	public int getTipoBatalla() {
 		return tipoBatalla;
 	}
-
-	public void setTipoBatalla(int tipoBatalla){
+	/**
+	 * setea el tipo de batalla
+	 */
+	public void setTipoBatalla(final int tipoBatalla){
 		this.tipoBatalla = tipoBatalla;
 	}
 }
