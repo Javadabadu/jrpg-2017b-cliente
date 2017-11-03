1f947fe734f8df051deaa0c161754896dee56551
Nicolás Casaux <nicocasaux@hotmail.com>
2017-11-03 20:29:18 -0300

    Checkstyle MenuInicio.java

--- src/main/java/frames/MenuInicio.java
+++ src/main/java/frames/MenuInicio.java
@@ -18,16 +18,57 @@ import javax.swing.border.EmptyBorder;
 import cliente.Cliente;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-
+/**
+ * @author Javadabadu
+ */
 public class MenuInicio extends JFrame {
 
 	private JPanel contentPane;
-
+	private static int x1 = 100;
+	private static int y1 = 100;
+	private static int width1 = 450;
+	private static int height1 = 300;
+	private static int x2 = 100;
+	private static int y2 = 100;
+	private static int width2 = 450;
+	private static int height2 = 300;
+	private static int x3 = 0;
+	private static int y3 = 0;
+	private static int width3 = 444;
+	private static int height3 = 271;
+	private static int x4 = 205;
+	private static int y4 = 162;
+	private static int width4 = 482;
+	private static int height4 = 23;
+	private static int x5 = 210;
+	private static int y5 = 202;
+	private static int width5 = 91;
+	private static int height5 = 23;
+	private static int x6 = 127;
+	private static int y6 = 162;
+	private static int width6 = 191;
+	private static int height6 = 23;
+	private static int x7 = 127;
+	private static int y7 = 202;
+	private static int width7 = 191;
+	private static int height7 = 23;
+	private static int x8 = 0;
+	private static int y8 = 0;
+	private static int width8 = 444;
+	private static int height8 = 271;
+	private static int top = 5;
+	private static int bottom = 5;
+	private static int left = 5;
+	private static int right = 5;
+	private static int font = 15;
+	/**
+	 * Constructor por defecto
+	 */
 	public MenuInicio() {
 		addKeyListener(new KeyAdapter() {
 			@Override
-			public void keyPressed(KeyEvent e) {
-				if(e.getKeyCode() == KeyEvent.VK_ENTER) {
+			public void keyPressed(final KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_ENTER) {
 					Cliente cliente = new Cliente();
 					cliente.start();
 					dispose();
@@ -37,81 +78,80 @@ public class MenuInicio extends JFrame {
 		setIconImage(Toolkit.getDefaultToolkit().getImage("src/main/java/frames/IconoWome.png"));
 		setCursor(Toolkit.getDefaultToolkit().createCustomCursor(
 				new ImageIcon(MenuJugar.class.getResource("/cursor.png")).getImage(),
-				new Point(0,0),"custom cursor"));
+				new Point(0, 0), "custom cursor"));
 
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
 		// Propiedades de la ventana
 		setTitle("WOME - World Of the Middle Earth");
-		setBounds(100, 100, 450, 300);
+		setBounds(x1, y1, width1, height1);
 		setLocationRelativeTo(null);
 		setResizable(false);
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setBorder(new EmptyBorder(top, left, bottom, right));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
 		JLabel lblLogo = new JLabel("");
 		lblLogo.setIcon(new ImageIcon(MenuCarga.class.getResource("/frames/WOME.png")));
-		lblLogo.setBounds(109, 39, 216, 90);
+		lblLogo.setBounds(x2, y2, width2, height2);
 		contentPane.add(lblLogo);
 
 
 		JLayeredPane layeredPane = new JLayeredPane();
-		layeredPane.setBounds(0, 0, 444, 271);
+		layeredPane.setBounds(x3, y3, width3, height3);
 		contentPane.add(layeredPane);
 
 		// Boton Jugar
 		JLabel lblRegistrarse = new JLabel("Jugar");
-		lblRegistrarse.setBounds(205, 162, 82, 23);
+		lblRegistrarse.setBounds(x4, y4, width4, height4);
 		layeredPane.add(lblRegistrarse, new Integer(2));
 		lblRegistrarse.setForeground(Color.WHITE);
 		lblRegistrarse.setEnabled(true);
-		lblRegistrarse.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblRegistrarse.setFont(new Font("Tahoma", Font.PLAIN, font));
 		lblRegistrarse.setBackground(Color.WHITE);
 
 		// Boton Salir
 		JLabel lblIniciarSesion = new JLabel("Salir");
-		lblIniciarSesion.setBounds(210, 202, 91, 23);
+		lblIniciarSesion.setBounds(x5, y5, width5, height5);
 		layeredPane.add(lblIniciarSesion, new Integer(2));
 		lblIniciarSesion.setForeground(Color.WHITE);
-		lblIniciarSesion.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblIniciarSesion.setFont(new Font("Tahoma", Font.PLAIN, font));
 
 		JButton btnRegistrar = new JButton("Jugar");
-		btnRegistrar.setBounds(127, 162, 191, 23);
+		btnRegistrar.setBounds(x6, y6, width6, height6);
 		layeredPane.add(btnRegistrar, new Integer(1));
 		btnRegistrar.setFocusable(false);
 		btnRegistrar.setIcon(new ImageIcon(MenuJugar.class.getResource("/frames/BotonMenu.png")));
 		btnRegistrar.addActionListener(new ActionListener() {
 			@Override
-			public void actionPerformed(ActionEvent e) {
+			public void actionPerformed(final ActionEvent e) {
 				Cliente cliente = new Cliente();
 				cliente.start();
 				dispose();
 			}
 		});
-		
 		JButton btnIniciarSesion = new JButton("Salir");
-		btnIniciarSesion.setBounds(127, 202, 191, 23);
+		btnIniciarSesion.setBounds(x7, y7, width7, height7);
 		layeredPane.add(btnIniciarSesion, new Integer(1));
 		btnIniciarSesion.setFocusable(false);
 		btnIniciarSesion.setIcon(new ImageIcon(MenuJugar.class.getResource("/frames/BotonMenu.png")));
 		btnIniciarSesion.addActionListener(new ActionListener() {
 			@Override
-			public void actionPerformed(ActionEvent e) {
+			public void actionPerformed(final ActionEvent e) {
 				dispose();
 			}
 		});
-		
 		JLabel lblBackground = new JLabel("");
-
-		lblBackground.setBounds(0, 0, 444, 271);
+		lblBackground.setBounds(x8, y8, width8, height8);
 		lblBackground.setIcon(new ImageIcon(MenuJugar.class.getResource("/frames/menuBackground.jpg")));
 		layeredPane.add(lblBackground, new Integer(0));
 	}
-
-	public static void main(String[] args) {
+	/**
+	 * 
+	 * Main
+	 */
+	public static void main(final String[] args) {
 		new MenuInicio().setVisible(true);
 	}
-
 }
