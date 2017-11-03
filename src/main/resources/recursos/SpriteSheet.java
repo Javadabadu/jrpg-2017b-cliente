60f9c210d6c256d204ec8f01d4f098a4fd74ae91
Nicolás Casaux <nicocasaux@hotmail.com>
2017-11-03 10:57:03 -0300

    Checkstyle SpriteSheet.java

--- src/main/resources/recursos/SpriteSheet.java
+++ src/main/resources/recursos/SpriteSheet.java
@@ -1,16 +1,21 @@
 package recursos;
 
 import java.awt.image.BufferedImage;
-
+/**
+ * @author Javadabadu
+ */
 public class SpriteSheet {
-
 	private BufferedImage sprite;
-	
-	public SpriteSheet(BufferedImage sprite) {
+	/**
+	 * Constructor
+	 */
+	public SpriteSheet(final BufferedImage sprite) {
 		this.sprite = sprite;
 	}
-	
-	public BufferedImage getTile(int x, int y, int ancho, int alto) {
+	/**
+	 * obtiene el Tile
+	 */
+	public BufferedImage getTile(final int x,final int y,final int ancho,final int alto) {
 		return sprite.getSubimage(x, y, ancho, alto);
 	}
 }
