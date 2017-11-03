57b0f3e01b9b53b88d6023cf91146deb60f7f085
Nicolás Casaux <nicocasaux@hotmail.com>
2017-11-03 13:35:24 -0300

    Checkstyle PaqueteMovimiento.java

--- src/main/java/mensajeria/PaqueteMovimiento.java
+++ src/main/java/mensajeria/PaqueteMovimiento.java
@@ -1,7 +1,9 @@
 package mensajeria;
 
 import java.io.Serializable;
-
+/**
+ * @author Javadabadu
+ */
 public class PaqueteMovimiento extends Paquete implements Serializable, Cloneable {
 
 	private int id;
@@ -9,63 +11,88 @@ public class PaqueteMovimiento extends Paquete implements Serializable, Cloneabl
 	private float posY;
 	private int direccion;
 	private int frame;
-
+	/**
+	 * Constructor por defecto
+	 */
 	public PaqueteMovimiento() {
 		setComando(Comando.MOVIMIENTO);
 	}
-
-	public PaqueteMovimiento(int idPersonaje) {
+	/**
+	 * Constructor con id
+	 */
+	public PaqueteMovimiento(final int idPersonaje) {
 		id = idPersonaje;
 		setComando(Comando.MOVIMIENTO);
 	}
-
-	public PaqueteMovimiento(int idPersonaje, float posX, float posY) {
+	/**
+	 * Constructor parametrizado
+	 */
+	public PaqueteMovimiento(final int idPersonaje, final float posX, final float posY) {
 		this.id = idPersonaje;
 		this.posX = posX;
 		this.posY = posY;
 		setComando(Comando.MOVIMIENTO);
 	}
-
+	/**
+	 * obtiene el id del personaje
+	 */
 	public int getIdPersonaje() {
 		return id;
 	}
-
-	public void setIdPersonaje(int idPersonaje) {
+	/**
+	 * setea el id del personaje
+	 */
+	public void setIdPersonaje(final int idPersonaje) {
 		this.id = idPersonaje;
 	}
-
+	/**
+	 * obtiene la posicion en X
+	 */
 	public float getPosX() {
 		return posX;
 	}
-
-	public void setPosX(float posX) {
+	/**
+	 * setea la posicion en X
+	 */
+	public void setPosX(final float posX) {
 		this.posX = posX;
 	}
-
+	/**
+	 * obtiene la posicion en Y
+	 */
 	public float getPosY() {
 		return posY;
 	}
-
-	public void setPosY(float posY) {
+	/**
+	 * setea la posicion en Y
+	 */
+	public void setPosY(final float posY) {
 		this.posY = posY;
 	}
-
+	/**
+	 * obtiene la direccion
+	 */
 	public int getDireccion() {
 		return direccion;
 	}
-
-	public void setDireccion(int direccion) {
+	/**
+	 * setea la direccion
+	 */
+	public void setDireccion(final int direccion) {
 		this.direccion = direccion;
 	}
-
+	/**
+	 * obtiene el frame
+	 */
 	public int getFrame() {
 		return frame;
 	}
-
-	public void setFrame(int frame) {
+	/**
+	 * setea el frame
+	 */
+	public void setFrame(final int frame) {
 		this.frame = frame;
 	}
-
 	@Override
 	public Object clone() {
 		Object obj = null;
