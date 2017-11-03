b1022b5c7065226c2d755b41948eb5849fd5b08c
Nicolás Casaux <nicocasaux@hotmail.com>
2017-11-02 21:07:46 -0300

    Checkstyle Registro.java

--- src/main/java/comandos/Registro.java
+++ src/main/java/comandos/Registro.java
@@ -4,30 +4,26 @@ import javax.swing.JOptionPane;
 
 import frames.MenuCreacionPj;
 import mensajeria.Paquete;
-
+/**
+ * @author Javadabadu
+ * 
+ */
 public class Registro extends ComandosCliente {
 
 	@Override
 	public void ejecutar() {
 		synchronized (this) {
-			
-		
 			Paquete paquete = (Paquete) gson.fromJson(cadenaLeida, Paquete.class);
 			if (paquete.getMensaje().equals(Paquete.msjExito)) {
 
 				// Abro el menu para la creaci�n del personaje
-				MenuCreacionPj menuCreacionPJ = new MenuCreacionPj(cliente, cliente.getPaquetePersonaje(),gson);
+				MenuCreacionPj menuCreacionPJ =
+						new MenuCreacionPj(cliente, cliente.getPaquetePersonaje(), gson);
 				menuCreacionPJ.setVisible(true);
 
 				// Espero a que el usuario cree el personaje
-
 				// Recibo el paquete personaje con los datos (la id incluida)
-
-				
-
 				// Indico que el usuario ya inicio sesion
-				
-
 			} else {
 				if (paquete.getMensaje().equals(Paquete.msjFracaso)) {
 					JOptionPane.showMessageDialog(null, "No se pudo registrar.");
